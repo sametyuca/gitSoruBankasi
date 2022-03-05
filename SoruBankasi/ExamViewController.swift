d33dc518f7598cefdf1872843c42935c39acff70
samet yuca <samet_yuca@hotmail.com>
2022-03-06 00:05:52 +0300

    first commit

---
+++ SoruBankasi/ExamViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ExamViewController.swift
+//  SoruBankasi
+//
+//  Created by MacBook Air on 24.12.2021.
+//
+
+import UIKit
+
+class ExamViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
